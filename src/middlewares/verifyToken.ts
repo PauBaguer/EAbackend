ac3ad9d1eed1b0713c935e3549c9ec7766ffbbdb
Pau Baguer <pauinki@gmail.com>
2022-03-31 01:25:30 +0200

    Added JWT token based authentication

---
+++ src/middlewares/verifyToken.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { UserModel } from "../models/user.js";
+
+export const VerifyToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.headers["x-access-token"];
+
+    console.log(token);
+    if (!token) {
+      res.status(403).send({ message: "Token not provided" });
+      return;
+    }
+    if (!(typeof token === "string")) throw "Token not a string";
+
+    const SECRET = process.env.JWT_SECRET;
+    let decoded;
+    try {
+      decoded = jwt.verify(token!, SECRET!);
+    } catch (e) {
+      res.status(403).send({ message: "User not authorized" });
+      return;
+    }
+
+    console.log(decoded!);
+
+    const user = await UserModel.findOne({ _id: decoded!.id, disabled: false });
+    if (!user) {
+      res.status(403).send({ message: "User not authorized" });
+      return;
+    }
+  } catch (e) {
+    res.status(500).send({ message: `Server error: ${e}` });
+    return;
+  }
+  next();
+};
