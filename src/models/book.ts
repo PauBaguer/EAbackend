6cfd828bea57118cbbe8116329b8d16e0a313ed6
Pau Baguer <pauinki@gmail.com>
2022-04-05 17:41:02 +0200

    ts and dates in all entities

--- src/models/book.ts
+++ src/models/book.ts
@@ -1,19 +1,21 @@
 import mongoose, { Document } from "mongoose";
+import Dates from "./dates";
 const Schema = mongoose.Schema;
 const model = mongoose.model;
 
-export interface Book extends Document {
-    title: String;
-    ISBN: String;
-    photoURL: String[];
-    description: String;
-    publishedDate: Date;
-    editorial: String;
-    rate: Number;
-    categories: String[];
+export interface Book extends Document, Dates {
+  title: String;
+  ISBN: String;
+  photoURL: String[];
+  description: String;
+  publishedDate: Date;
+  editorial: String;
+  rate: Number;
+  categories: String[];
 }
 
-const bookSchema = new Schema({
+const bookSchema = new Schema(
+  {
     title: { type: String, required: true },
     categories: { type: [String], required: true },
     ISBN: { type: String, required: true, unique: true },
@@ -23,8 +25,9 @@ const bookSchema = new Schema({
     description: { type: String, required: true },
     location: { latitude: { type: Number }, longitude: { type: Number } },
     rate: { type: Number },
-    editorial: { type: String }
-},
-    { timestamps: true });
+    editorial: { type: String },
+  },
+  { timestamps: true }
+);
 
-export const BookModel = mongoose.model('Book', bookSchema);
+export const BookModel = mongoose.model("Book", bookSchema);
