7287c469764a2f58e802734b5317aac69f339ae7
Pau.rb <80818194+Paudenn@users.noreply.github.com>
2022-06-25 10:43:58 +0200

    test

--- src/models/rate.ts
+++ src/models/rate.ts
@@ -4,20 +4,20 @@ const Schema = mongoose.Schema;
 const model = mongoose.model;
 
 export interface Rate extends Document {
-  bookId: String;
-  rating: { type: { userId: String; rate: Number } }[];
-  totalRate: Number;
+  bookId: string;
+  rating: { type: { userId: string; rate: number } }[];
+  totalRate: number;
 }
 
 const rateSchema = new Schema(
-  {
+    {
     bookId: { type: String, required: true },
     rating: [
       {
         userId: { type: String, required: true },
         rate: { type: Number, required: true },
       },
-    ],
+       ],
     totalRate: { type: Number, required: true },
   },
   { timestamps: true }
